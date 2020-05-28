6b74c5823c54fd267004e67ef0b5d3938f8ab96d
Yousef Aldawoud <yousef.l.t.aldawoud@gmail.com>
2020-05-28 18:05:52 +1200

    made test for post validation

---
+++ tests/Feature/PostValidateTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+use App\Post;
+
+class PostValidateTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testValidateSubmittedPost()
+    {
+        $user = factory(User::class)->create();
+        $user->assignRole("moderator");
+        $post = new Post;
+        $post->user_id = $user->id;
+        $post->status = "validation";
+        $post->save();
+        $response = $this->actingAs($user)
+        ->json('POST', route("post-validate",$post->id), ["status"=>"rejected"]);
+        $response->assertJson(["status"=>"success"]);
+        $response->assertStatus(200);
+
+    }
+
+    public function testValidateDraftPost()
+    {
+        $user = factory(User::class)->create();
+        $user->assignRole("moderator");
+        $post = new Post;
+        $post->user_id = $user->id;
+        $post->status = "draft";
+        $post->save();
+        $response = $this->actingAs($user)
+        ->json('POST', route("post-validate",$post->id), ["status"=>"rejected"]);
+        $response->assertJson(["status"=>"failed"]);
+        $response->assertStatus(200);
+
+    }
+}

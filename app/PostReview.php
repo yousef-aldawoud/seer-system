205e4b583f4c8ea95441a0fef14ce7c1fda7afa2
Yousef Aldawoud <yousef.l.t.aldawoud@protonmail.com>
2020-06-02 14:42:12 +1200

    made a post review test

--- app/PostReview.php
+++ app/PostReview.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostReview extends Model
 {
+    protected $fillable = [
+        'rating', 'comment', 'user_id',
+    ];
+
     public function user(){
         return $this->belongsTo('App\User');
     }
---
+++ tests/Feature/PostReviewTest.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+use App\Post;
+
+class PostReviewTest extends TestCase
+{
+    public function testCreateReview()
+    {
+        $postInfo=[
+            'title' => 'Post title 1',
+            "description"=>"Post description test",
+            "content"=>"Post description content",
+        ];
+
+        $postReviewInfo = [
+            'rating'=>4,
+            'comment'=>'Great post',
+        ];
+
+        $user = factory(User::class)->create();
+        $reviewer = factory(User::class)->create();
+
+        $post = new Post;
+        $post->user_id = $user->id;
+        $post->title = $postInfo['title'];
+        $post->description = $postInfo['description'];
+        $post->content = $postInfo['content'];
+        $post->save();
+        $response = $this->actingAs($reviewer)
+        ->json('POST', route("post-review-create",$post->id), $postReviewInfo);
+        $this->assertDatabaseHas('post_reviews', $postReviewInfo);
+        $post->reviews()->delete();
+        $post->delete();
+
+        
+    }
+
+    public function testDuplicateReview()
+    {
+        $postInfo=[
+            'title' => 'Post title 1',
+            "description"=>"Post description test",
+            "content"=>"Post description content",
+        ];
+
+        $postReviewInfo = [
+            'rating'=>4,
+            'comment'=>'Great post',
+        ];
+
+        $user = factory(User::class)->create();
+        $reviewer = factory(User::class)->create();
+
+        $post = new Post;
+        $post->user_id = $user->id;
+        $post->title = $postInfo['title'];
+        $post->description = $postInfo['description'];
+        $post->content = $postInfo['content'];
+        $post->save();
+        $this->actingAs($reviewer)
+        ->json('POST', route("post-review-create",$post->id), $postReviewInfo);
+        $this->actingAs($reviewer)
+        ->json('POST', route("post-review-create",$post->id), $postReviewInfo);
+        $reviewCount = $post->reviews()->where('user_id',$reviewer->id)->count();
+        $this->assertTrue($reviewCount === 1);
+        $post->reviews()->delete();
+        $post->delete();
+    }
+
+    public function testChangeReview()
+    {
+        $postInfo=[
+            'title' => 'Post title 1',
+            "description"=>"Post description test",
+            "content"=>"Post description content",
+        ];
+
+        $postReviewInfo = [
+            'rating'=>4,
+            'comment'=>'Great post',
+        ];
+
+        $user = factory(User::class)->create();
+        $reviewer = factory(User::class)->create();
+        $postReviewInfo['user_id']=$reviewer->id;
+        $post = new Post;
+        $post->user_id = $user->id;
+        $post->title = $postInfo['title'];
+        $post->description = $postInfo['description'];
+        $post->content = $postInfo['content'];
+        $post->save();
+        $review = $post->reviews()->create($postReviewInfo);
+        $postReviewInfo['comment'] = 'not good';
+        $postReviewInfo['rating'] = 1;
+        $this->actingAs($reviewer)
+        ->json('POST', route("post-review-update",$review->id), $postReviewInfo);
+        $this->assertDatabaseHas('post_reviews', $postReviewInfo);
+        $post->reviews()->delete();
+        $post->delete();
+    }
+
+    public function testDeleteReview()
+    {
+
+        $user = factory(User::class)->create();
+        $reviewer = factory(User::class)->create();
+        $postInfo=[
+            'title' => 'Post title 1',
+            "description"=>"Post description test",
+            "content"=>"Post description content",
+        ];
+        $postReviewInfo = [
+            'rating'=>4,
+            'comment'=>'Great post',
+        ];
+        $post = new Post;
+        $post->user_id = $user->id;
+        $post->update($postInfo);
+        $post->save();
+        $postReviewInfo['user_id']=$reviewer->id;
+        $review = $post->reviews()->create($postReviewInfo);
+        $route = route("post-review-delete",$post->id);
+        $response = $this->actingAs($user)
+        ->json('DELETE', $route);
+        $this->assertDeleted("post_reviews",["id"=>$review->id]);
+        $response->assertJson(["status"=>"success"]);
+
+    }
+
+}
